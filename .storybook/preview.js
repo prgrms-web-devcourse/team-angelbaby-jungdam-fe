3da3cbcb0aa177070d4c63e432e0f83accdf2cb3
minsgy <alstjrchl97@naver.com>
2021-12-06 02:19:14 +0900

    📦 chore #5 : customViewPort 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,27 @@
+const customViewports = {
+  mobile360: {
+    name: 'Mobile 360px',
+    styles: {
+      width: '360px',
+      height: '640px'
+    }
+  },
+  mobile375: {
+    name: 'Mobile 375px',
+    styles: {
+      width: '375px',
+      height: '667px'
+    }
+  },
+  mobile414: {
+    name: 'Mobile 414px',
+    styles: {
+      width: '414px',
+      height: '736px'
+    }
+  }
+}
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +30,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: { viewports: customViewports},
 }

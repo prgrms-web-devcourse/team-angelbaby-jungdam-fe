f2aaf6ae4af86c2b718436d327498fb75da0ab5a
minsgy <alstjrchl97@naver.com>
2021-12-07 01:26:52 +0900

    ✨ feat #17 : Divider Storybook setting

---
+++ src/stories/components/Divider.stories.jsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import font from '@assets/fonts/Font';
+import { Divider } from '@components/base';
+
+const Title = styled.h1`
+  ${font.heading_24};
+`;
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/Divider',
+  component: Divider,
+};
+
+export const Default = () => {
+  return (
+    <>
+      <Title>헤더1</Title>
+      <Divider />
+      <p>Content</p>
+    </>
+  );
+};

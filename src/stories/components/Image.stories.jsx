3774ebfc38ed9ea69260c46ea879c95b81ce0a2e
minsgy <alstjrchl97@naver.com>
2021-12-07 02:03:33 +0900

    ✨ feat #20 : Image Storybook testing

---
+++ src/stories/components/Image.stories.jsx
@@ -0,0 +1,54 @@
+import { Image } from '@components/base';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Components/Image',
+  component: Image,
+};
+
+export const Default = () => {
+  return (
+    <Image
+      src="https://picsum.photos/300/600"
+      width="200px"
+      height="200px"
+      alt="Image"
+    />
+  );
+};
+
+export const Cover = () => {
+  return (
+    <Image
+      src="https://picsum.photos/300/600"
+      width="200px"
+      height="200px"
+      alt="Image"
+      mode="cover"
+    />
+  );
+};
+
+export const Fill = () => {
+  return (
+    <Image
+      src="https://picsum.photos/300/600"
+      width="200px"
+      height="200px"
+      alt="Image"
+      mode="fill"
+    />
+  );
+};
+
+export const Contain = () => {
+  return (
+    <Image
+      src="https://picsum.photos/300/600"
+      width="200px"
+      height="200px"
+      alt="Image"
+      mode="contain"
+    />
+  );
+};

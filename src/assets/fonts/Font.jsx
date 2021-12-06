7ec696fc8ae9c974f72d0177f2f67d7b6ae50e37
minsgy <alstjrchl97@naver.com>
2021-12-06 18:26:27 +0900

    ♻️ refactor #10 : color, font 최종 에셋 적용

--- src/assets/fonts/Font.jsx
+++ src/assets/fonts/Font.jsx
@@ -1,28 +1,28 @@
 import { css } from 'styled-components';
 
 const font = {
-  Text1_18px: css`
-    font-size: 18px;
-    line-height: 26px;
-    font-weight: 700;
+  heading_24: css`
+    font-size: 24px;
+    line-height: 28px;
+    font-weight: 600;
   `,
 
-  Text1_14px: css`
-    font-size: 14px;
-    line-height: 20px;
-    font-weight: 700;
-  `,
-
-  Text1_12px: css`
-    font-size: 12px;
-    line-height: 18px;
-    font-weight: 700;
-  `,
-
-  Text2_12px: css`
-    font-size: 12px;
-    line-height: 18px;
+  heading_20: css`
+    font-size: 20px;
+    line-height: 24px;
     font-weight: 400;
+  `,
+
+  content_18: css`
+    font-size: 18px;
+    line-height: 22px;
+    font-weight: 400;
+  `,
+
+  content_16: css`
+    font-size: 16px;
+    line-height: 20px;
+    font-weight: 300;
   `,
 
   Text2_11px: css`

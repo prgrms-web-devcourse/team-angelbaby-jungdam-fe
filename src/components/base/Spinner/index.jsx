0cb9fec3a16f4ac9488b8e0407e0f8c13dde7654
minsgy <alstjrchl97@naver.com>
2021-12-07 16:20:03 +0900

    ✨ feat #21 : Spinner Component 설계

---
+++ src/components/base/Spinner/index.jsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import colors from '@assets/colors/Color';
+const Icon = styled.i`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const Spinner = ({ size = 16, color = colors.brown, isLoading = true }) => {
+  const sizeStyle = {
+    width: size,
+    height: size,
+  };
+
+  return isLoading ? (
+    <Icon>
+      <svg
+        width="38"
+        height="38"
+        viewBox="0 0 38 38"
+        xmlns="http://www.w3.org/2000/svg"
+        stroke={color}
+        style={sizeStyle}
+      >
+        <g fill="none" fill-rule="evenodd">
+          <g transform="translate(1 1)" stroke-width="2">
+            <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+            <path d="M36 18c0-9.94-8.06-18-18-18">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="1s"
+                repeatCount="indefinite"
+              />
+            </path>
+          </g>
+        </g>
+      </svg>
+    </Icon>
+  ) : null;
+};
+
+export default Spinner;

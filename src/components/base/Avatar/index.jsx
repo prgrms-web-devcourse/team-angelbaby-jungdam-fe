d7a791943f45dae0f326cf5e2483b2ad4c08d4fb
krungy <furler711@gmail.com>
2021-12-07 15:31:22 +0900

    ✨ feat #19 : Avatar Component 생성

---
+++ src/components/base/Avatar/index.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import color from '@assets/colors/Color';
+
+const Sizes = {
+  large: '80px',
+  base: '32px',
+};
+
+const AvatarWrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${({ size }) => Sizes[size]};
+  height: ${({ size }) => Sizes[size]};
+  border: 1px solid ${color.grey_50};
+  text-align: center;
+  border-radius: 50%;
+  overflow: hidden;
+  > img {
+    width: 100%;
+  }
+`;
+
+const Avatar = ({ size, src, ...props }) => {
+  return (
+    <AvatarWrapper {...props} size={size}>
+      <img src={src} alt="userProfile" />
+    </AvatarWrapper>
+  );
+};
+
+Avatar.defaultProps = {
+  size: 'base',
+  src: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+};
+
+Avatar.propTypes = {
+  size: PropTypes.oneOf(['base', 'large']),
+  src: PropTypes.string,
+};
+
+export default Avatar;
--- src/components/base/index.jsx
+++ src/components/base/index.jsx
@@ -1,3 +1,3 @@
 export { default as Divider } from './Divider';
 export { default as Image } from './Image';
-
+export { default as Avatar } from './Avatar';

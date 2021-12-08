5e27594a05e06ff721ec88f7e0d858c720caae62
krungy <furler711@gmail.com>
2021-12-09 02:07:18 +0900

    ♻️ refactor #51 : width 기본값 설정 $ font-size 크기 조절

--- src/components/base/Input/index.jsx
+++ src/components/base/Input/index.jsx
@@ -1,22 +1,24 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import color from '@assets/colors';
+import font from '@assets/fonts';
 
 const inputSizes = {
-  sm: '24px',
-  base: '32px',
+  sm: `${font.content_12}`,
+  base: `${font.content_16}`,
+  lg: `${font.content_18}`,
 };
 
 const StyledInput = styled.input`
   display: block;
-  height: ${({ size }) => inputSizes[size]};
+  width: ${({ width }) => width};
   box-sizing: border-box;
   border: none;
   border-bottom: 1px solid ${color.brown};
-  font-size: 16px;
   background-color: transparent;
   color: ${color.black};
   outline: none;
+  ${({ size }) => inputSizes[size]};
 
   &::placeholder {
     color: ${color.grey};
@@ -38,20 +40,7 @@ const StyledInput = styled.input`
   }
 `;
 
-const Input = ({
-  width,
-  isFullWidth,
-  size,
-  onChange,
-  name,
-  error,
-  success,
-  ...props
-}) => {
-  const inputStyle = {
-    width: isFullWidth ? `100%` : `${width}px`,
-  };
-
+const Input = ({ width, onChange, size, name, error, success, ...props }) => {
   let status = '';
 
   if (error) {
@@ -66,27 +55,27 @@ const Input = ({
       onChange={onChange}
       name={name}
       size={size}
+      width={width}
       className={status || undefined}
       {...props}
-      style={{ ...props.style, ...inputStyle }}
+      style={{ ...props.style }}
     />
   );
 };
 
 Input.propTypes = {
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  width: PropTypes.string,
   isFullWidth: PropTypes.bool,
   onChange: PropTypes.func,
   name: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['base', 'sm']),
+  size: PropTypes.oneOf(['sm', 'base', 'lg']),
   error: PropTypes.bool,
   success: PropTypes.bool,
   style: PropTypes.object,
 };
 
 Input.defaultProps = {
-  width: 288,
-  isFullWidth: false,
+  width: '100%',
   onChange: () => {},
   size: 'base',
   error: false,

39ffaa4c4bbb04d5929c5b662dd69c9723ee217b
krungy <furler711@gmail.com>
2021-12-08 13:52:55 +0900

    ✨ feat #36 : Input Storybook 생성

---
+++ src/stories/components/Input.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Input } from '@components/base';
+
+export default {
+  title: 'Components/Input',
+  component: Input,
+};
+
+export const Default = (args) => {
+  return <Input {...args} type="text" placeholder="내용을 입력해주세요." />;
+};
+
+export const Date = (args) => {
+  return <Input {...args} type="date" placeholder="날짜를 입력해주세요." />;
+};
+
+export const Small = (args) => {
+  return (
+    <Input {...args} type="text" size="sm" placeholder="내용을 입력해주세요." />
+  );
+};
+
+export const Error = (args) => {
+  return <Input {...args} error type="text" placeholder="Error" />;
+};
+
+export const Success = (args) => {
+  return <Input {...args} success type="text" placeholder="Success" />;
+};
+
+export const Disabled = (args) => {
+  return <Input {...args} type="text" placeholder="Disabled" disabled />;
+};

bc4e11b868b02c44c722e8c9da6b3db2bd0833dd
krungy <furler711@gmail.com>
2021-12-09 02:11:59 +0900

    ♻️ refactor #51 : 필요없는 Storybook 컴포넌트 제거

--- src/stories/components/Input.stories.jsx
+++ src/stories/components/Input.stories.jsx
@@ -14,12 +14,6 @@ export const Date = (args) => {
   return <Input {...args} type="date" placeholder="날짜를 입력해주세요." />;
 };
 
-export const Small = (args) => {
-  return (
-    <Input {...args} type="text" size="sm" placeholder="내용을 입력해주세요." />
-  );
-};
-
 export const Error = (args) => {
   return <Input {...args} error type="text" placeholder="Error" />;
 };

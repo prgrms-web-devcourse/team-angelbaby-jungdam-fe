1085c562095132460c2b779814cdba9ae34b3473
MyeongHoonNam <codenmh0822@naver.com>
2021-12-09 16:41:30 +0900

    ♻️ refactor #25 : storybook emotion build 코드 축소

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,6 @@
 const customWebpackConfig = require('../craco.config.js');
 const path = require('path');
-const fs = require('fs');
-
-function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath));
-
-  while (true) {
-    if (fs.existsSync(path.join(currDir, 'package.json'))) {
-      return currDir;
-    }
-
-    const { dir, root } = path.parse(currDir);
-
-    if (dir === root) {
-      throw new Error(
-        `Could not find package.json in the parent directories starting from ${filepath}.`,
-      );
-    }
-
-    currDir = dir;
-  }
-}
+const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -29,7 +9,6 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     'storybook-mobile',
-    'storybook-preset-craco',
   ],
   webpackFinal: async (config) => {
     const { webpack } = customWebpackConfig;
@@ -41,8 +20,8 @@ module.exports = {
         alias: {
           ...config.resolve.alias,
           ...webpack.alias,
-          '@emotion/core': getPackageDir('@emotion/react'),
-          '@emotion/styled': getPackageDir('@emotion/styled'),
+          '@emotion/react': toPath('node_modules/@emotion/react'),
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
         },
       },
     };

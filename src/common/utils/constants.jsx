9a5f5ba360258606c5575162de0d90bd3df315c7
minsgy <alstjrchl97@naver.com>
2021-12-11 03:27:19 +0900

    🔥 remove #48 : 불필요한 코드삭제

--- src/common/utils/constants.jsx
+++ src/common/utils/constants.jsx
@@ -1,3 +1,2 @@
 export const isProduction = process.env.REACT_APP_MODE === 'production';
 export const API_POINT = !isProduction ? process.env.REACT_APP_API_URL : 'none';
-export const HOME_PAGE = '/';

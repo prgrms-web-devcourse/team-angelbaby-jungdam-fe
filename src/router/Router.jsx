f81110c417d7caef5cad05492cd2c419240bcafb
minsgy <alstjrchl97@naver.com>
2021-12-11 03:28:07 +0900

    ✨ feat #48 : 인증 라우터 및 v6 react-router 적용

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,7 @@
 import React, { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DefaultTemplate from '@styles/DefaultTemplate';
 import ScrollToTop from '@utils/ScrollToTop';
+import { AuthRoute, PreventedRoute } from '@router';
 import { LandingPage, LoginPage, MainPage, OAuthRedirect } from '@pages';
 
 const Router = () => {
@@ -9,10 +10,19 @@ const Router = () => {
       <ScrollToTop />
       <DefaultTemplate>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/oauth/redirect" element={<OAuthRedirect />} />
-          <Route path="/tutorial" element={<LandingPage />} />
-          <Route path="/" element={<MainPage />} />
+          {/* PreventedRoute - Token이 존재 할 시 자동 메인(앨범 선택)으로 이동 */}
+          <Route element={<PreventedRoute />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/oauth/redirect" element={<OAuthRedirect />} />
+            <Route path="/tutorial" element={<LandingPage />} />
+          </Route>
+          {/* AuthRoute - Token이 존재해야 접속 가능함. */}
+          <Route element={<AuthRoute />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/album" element={<MainPage />} />
+          </Route>
+          {/* 임시 404 페이지 */}
+          <Route path="*" element={<div>404 Page</div>} />
         </Routes>
       </DefaultTemplate>
     </BrowserRouter>

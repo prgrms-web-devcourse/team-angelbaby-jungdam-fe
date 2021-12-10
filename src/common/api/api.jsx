a9612f580abdefa6c066d3b66d63e1237a5dc80d
minsgy <alstjrchl97@naver.com>
2021-12-11 03:26:30 +0900

    ✨ feat #48 : Bearer Auth token 임시 설정

--- src/common/api/api.jsx
+++ src/common/api/api.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-import { API_POINT } from '@utils/constants';
+
 // env 파일 읽어오기 + axios 베이스 URL 설정
 // CRA에서 환경 변수 사용하기 위해서는 REACT_APP_이 접두어로 꼭 붙어야함.
-dotenv.config();
-axios.defaults.baseURL = API_POINT;
+
+// dotenv.config();
+// axios.defaults.baseURL = API_POINT;
 
 const Api = ({
   url,
@@ -18,13 +18,15 @@ const Api = ({
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
     'Access-Control-Allow-Headers':
       'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
-    mode: 'cors',
   };
 
-  // if (accessToken !== null) {
-  //   headers.Authorization = `Bearer ${accessToken}`;
-  // }
+  // 토큰이 없을 시 header 값에 token이 포함되지 않음.
+  const token = sessionStorage.getItem('token');
 
+  if (token !== null) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  console.log(url, token, headers);
   return axios({
     method: type,
     url: `${url}`,

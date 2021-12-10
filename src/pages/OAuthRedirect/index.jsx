2fca377856365c08b40b8f64b91bf666f1da3f6a
minsgy <alstjrchl97@naver.com>
2021-12-11 03:28:58 +0900

    ✨feat #48 : OAuth Redirect Page 제작 및 로직 완료

--- src/pages/OAuthRedirect/index.jsx
+++ src/pages/OAuthRedirect/index.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import QueryString from 'qs';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { member, fetchMemberLogin } from '@redux/member';
 
 const OAuthRedirect = () => {
+  const { setToken } = member.actions;
+  const dispatch = useDispatch();
   const { search } = useLocation();
-  const navigate = useNavigate();
   const { token } = QueryString.parse(search, {
     ignoreQueryPrefix: true,
   });
-
+  const navigate = useNavigate();
   useEffect(() => {
     if (token) {
-      // navigate('/');
-      // Member 데이터 요청 => 정상 요청 시 데이터 저장 후 MainRedirect
-      // ReduxStore 토큰 저장 + 멤버 정보 저장
-      // 에러 요청 시 => 에러 메시지 출력 후 로그인 페이지로 이동
-      console.log('토큰 있음');
+      try {
+        dispatch(setToken(token));
+        sessionStorage.setItem('token', token);
+        dispatch(fetchMemberLogin());
+        navigate('/');
+      } catch (error) {
+        alert(error);
+      }
     } else {
-      // navigate('/login');
-      console.log('토큰 없음!');
+      alert('인증API 실패');
+      navigate('/login');
     }
-  }, [token, navigate]);
+  }, [dispatch, navigate, token, setToken]);
 
   return <></>;
 };

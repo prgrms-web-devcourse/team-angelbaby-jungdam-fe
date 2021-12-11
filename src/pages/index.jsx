205c6d8c0442162d36b835e3c672ddf04ace2107
krungy <furler711@gmail.com>
2021-12-11 16:55:16 +0900

    ✨ feat #82 : UserCard 제외한 MemberListPage 작업 추가

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as MainPage } from './MainPage';
 export { default as LoginPage } from './LoginPage';
 export { default as LandingPage } from './LandingPage';
+export { default as MemberListPage } from './MemberListPage';

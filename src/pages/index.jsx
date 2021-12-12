da0f7ce66aef30f0bd7c256da50fddbe0b5d84f3
minsgy <alstjrchl97@naver.com>
2021-12-12 15:14:00 +0900

    🎨 style : AlbumMainPage Module

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as MainPage } from './MainPage';
 export { default as LoginPage } from './LoginPage';
 export { default as LandingPage } from './LandingPage';
+export { default as AlbumMainPage } from './AlbumMainPage';

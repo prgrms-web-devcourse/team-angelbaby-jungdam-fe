c538566efe3e4ddd4562577ed1e9b72482a64f17
minsgy <alstjrchl97@naver.com>
2021-12-12 15:12:16 +0900

    ✨ feat #44 : ImageDim 컴포넌트 기능 추가

--- src/components/base/index.jsx
+++ src/components/base/index.jsx
@@ -1,10 +1,10 @@
 export { default as Icon } from './Icon';
 export { default as Divider } from './Divider';
-export { default as Image } from './Image';
+export { default as Image, DimImage } from './Image';
 export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as Avatar } from './Avatar';
-export { default as Button } from './Button';
+export { default as Button, BottomFloatButton } from './Button';
 export { default as Spinner } from './Spinner';
 export { default as Skeleton } from './Skeleton';
 export { default as Lottie } from './Lottie';

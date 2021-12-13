9dc0c5fb2e04138ff98beaf67043787392060d41
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 16:03:13 +0900

    ✨ feat #80 : step 상태에 따른 ProgressBar UI 구현

--- src/pages/DiaryCreatePage/index.jsx
+++ src/pages/DiaryCreatePage/index.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Header, DiaryCreateStepOne } from '@components/domain';
 import { Button, Icon, ProgressBar } from '@components/base';
 import font from '@assets/fonts';
@@ -11,25 +12,6 @@ const DefaultMarginTop = css`
   margin: 80px 0 80px 0;
 `;
 
-const leftHeaderContent = () => {
-  return (
-    <>
-      <Button>
-        <Icon name="ep:back" color={color.brown} />
-      </Button>
-    </>
-  );
-};
-
-const centerHeaderContent = () => {
-  const Span = styled.span`
-    white-space: nowrap;
-    ${font.heading_20};
-  `;
-
-  return <Span>다이어리 만들기</Span>;
-};
-
 const ButtonStyle = {
   width: '100%',
   boxSizing: 'border-box',
@@ -38,6 +20,44 @@ const ButtonStyle = {
 };
 
 const DiaryCreatePage = () => {
+  const [step, setStep] = useState(1);
+  const navigate = useNavigate();
+
+  const handleNextButtonClick = () => {
+    setStep(() => step + 1);
+  };
+
+  const handlePrevButtonClick = () => {
+    if (step === 1) {
+      navigate('../diary');
+    } else {
+      setStep(() => step - 1);
+    }
+  };
+
+  const leftHeaderContent = () => {
+    return (
+      <>
+        <Button>
+          <Icon
+            name="ep:back"
+            color={color.brown}
+            onClick={handlePrevButtonClick}
+          />
+        </Button>
+      </>
+    );
+  };
+
+  const centerHeaderContent = () => {
+    const Span = styled.span`
+      white-space: nowrap;
+      ${font.heading_20};
+    `;
+
+    return <Span>다이어리 만들기</Span>;
+  };
+
   return (
     <DefaultContainer>
       <Header
@@ -45,14 +65,14 @@ const DiaryCreatePage = () => {
         centerComponent={centerHeaderContent()}
       />
 
-      <ProgressBar css={DefaultMarginTop} totalStep={3} currentStep={1} />
+      <ProgressBar css={DefaultMarginTop} totalStep={3} currentStep={step} />
 
       <DiaryCreateStepOne />
 
       <Button
         mode="primary"
         style={{ ...ButtonStyle }}
-        onClick={() => console.log('clickTest')}
+        onClick={handleNextButtonClick}
       >
         다음
       </Button>

92006e5601a41145044df93002bcf65c24286102
krungy <furler711@gmail.com>
2021-12-14 03:05:48 +0900

    ♻️ refactor #104 : ALBUM_SETTING_LIST 앨범 삭제 제거 / 이동경로 추가 & DIvider 컴포넌트 사용

--- src/pages/AlbumSettingsPage/index.jsx
+++ src/pages/AlbumSettingsPage/index.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import DefaultContainer from '@styles/DefaultContainer';
 import color from '@assets/colors';
 import font from '@assets/fonts';
 import { useNavigate } from 'react-router-dom';
+import { Modal, Divider } from '@components/base';
 import { DetailPageHeader } from '@components/domain';
 
 const ALBUM_SETTINGS_LIST = [
   {
-    name: '그룹 정보 수정',
-    to: '', // 추가 예정
-  },
-  {
-    name: '그룹 식제',
-    to: '', // 추가 예정
+    name: '앨범 정보 수정',
+    to: 'edit',
   },
 ];
 
@@ -24,39 +21,61 @@ const AlbumSettingsPageWrapper = styled(DefaultContainer)`
 `;
 
 const AlbumSettingsTitle = styled.div`
-  height: 32px;
   ${font.heading_20}
-  border-bottom: 1px solid ${color.grey};
 `;
 
 const SettingsList = styled.div`
   display: flex;
   align-items: center;
-  height: 42px;
-  border-bottom: 1px solid ${color.grey_50};
+  height: 32px;
 `;
 
 const AlbumSettingsPage = () => {
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
   const navigate = useNavigate();
 
   const albumSettings = (list) =>
     list.map(({ name, to }) => (
-      <SettingsList
-        onClick={() => {
-          navigate(to);
-        }}
-      >
-        {name}
-      </SettingsList>
+      <>
+        <SettingsList
+          onClick={() => {
+            handleToPage(to);
+          }}
+        >
+          {name}
+        </SettingsList>
+        <Divider size={6} />
+      </>
     ));
+
+  const handleToPage = (url) => {
+    navigate(url);
+  };
+
+  const OpenDeleteModal = () => {
+    setDeleteModalVisible(true);
+  };
+
+  const CloseDeleteModal = () => {
+    if (deleteModalVisible) {
+      setDeleteModalVisible(false);
+    }
+  };
 
   return (
     <AlbumSettingsPageWrapper>
       <DetailPageHeader pageTitle="앨범 설정" />
       <AlbumSettingsTitle>앨범 설정</AlbumSettingsTitle>
+      <Divider style={{ backgroundColor: color.grey }} />
       {albumSettings(ALBUM_SETTINGS_LIST)}
+      <SettingsList onClick={OpenDeleteModal}>앨범 삭제</SettingsList>
+      <Divider size={6} />
+      <Modal visible={deleteModalVisible} onClose={CloseDeleteModal}>
+        정말로 앨범을 삭제하시겠습니까?
+      </Modal>
     </AlbumSettingsPageWrapper>
   );
 };
+// Modal 의 예 버튼을 눌렀을 때 처리하는 이벤트 작성 필요
 
 export default AlbumSettingsPage;

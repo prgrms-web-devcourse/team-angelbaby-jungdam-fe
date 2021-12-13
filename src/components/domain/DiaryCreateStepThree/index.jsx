e875100ad05838050f421ebe4c4cec156308104b
MyeongHoonNam <codenmh0822@naver.com>
2021-12-13 19:45:22 +0900

    🐛 fix #80 : 다른 요소 렌더링을 방해하는 버그를 이미지 크기 지정으로 해결

--- src/components/domain/DiaryCreateStepThree/index.jsx
+++ src/components/domain/DiaryCreateStepThree/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useCallback } from 'react';
-import { Upload, Icon } from '@components/base';
+import { Upload, Icon, Image } from '@components/base';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import color from '@assets/colors';
+import font from '@assets/fonts';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -14,44 +15,70 @@ import 'swiper/components/pagination/pagination.min.css';
 
 SwiperCore.use([Pagination]);
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.span`
+  margin-bottom: 10px;
+
+  &.subtitle {
+    margin-bottom: 20px;
+    color: ${color.grey};
+    ${font.content_16};
+  }
+
+  ${font.heading_24};
+`;
+
 const IconStyle = css`
   cursor: pointer;
 `;
 
 const StyledSwiper = styled(Swiper)`
+  width: 100%;
+  height: 33vh;
+  margin-top: 10px;
   display: flex;
   justify-content: center;
   position: relative;
-  width: 100%;
-  height: 250px;
+  border-radius: 5px;
 `;
 
-const DiaryCreateStepThree = () => {
+const DiaryCreateStepThree = ({ onChange }) => {
   const [images, setImages] = useState(null);
-  const handleUploadImagesChange = useCallback((images) => {
-    const imageUrls = [];
+  const handleUploadImagesChange = useCallback(
+    (images) => {
+      const imageUrls = [];
 
-    for (let i = 0; i < images.length; i++) {
-      const image = images[i];
+      for (let i = 0; i < images.length; i++) {
+        const image = images[i];
 
-      const fileReader = new FileReader();
+        const fileReader = new FileReader();
 
-      fileReader.onload = () => {
-        console.log(fileReader);
-        imageUrls.push(fileReader.result);
-        setImages([...imageUrls]);
-      };
+        fileReader.onload = () => {
+          imageUrls.push(fileReader.result);
+          setImages([...imageUrls]);
+          onChange && onChange(imageUrls);
+        };
 
-      fileReader.readAsDataURL(image);
-    }
-  }, []);
+        fileReader.readAsDataURL(image);
+      }
+    },
+    [onChange],
+  );
 
   const swiperParams = {
     pagination: true,
   };
 
   return (
-    <>
+    <Container>
+      <Title>추억이 담긴 사진을 넣어보세요.</Title>
+      <Title className="subtitle">
+        여러장의 사진을 동시에 지정할 수 도 있어요 !
+      </Title>
       <Upload onChange={handleUploadImagesChange}>
         <Icon
           name="fluent:camera-add-24-regular"
@@ -65,16 +92,12 @@ const DiaryCreateStepThree = () => {
         <StyledSwiper {...swiperParams}>
           {images.map((image, index) => (
             <SwiperSlide key={index}>
-              <img
-                src={image}
-                style={{ width: '100%', height: '100%', borderRadius: '10px' }}
-                alt=""
-              />
+              <Image src={image} alt="image" width="100%" height="100%" />
             </SwiperSlide>
           ))}
         </StyledSwiper>
       )}
-    </>
+    </Container>
   );
 };
 

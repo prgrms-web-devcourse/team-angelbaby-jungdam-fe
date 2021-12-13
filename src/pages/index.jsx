1e1e706ce04de9299eb5d7c4c67ea32f9a00b4a0
minsgy <alstjrchl97@naver.com>
2021-12-14 00:16:54 +0900

    ✨ feat #78 : 앨범 생성 페이지 구현 및 모듈화 완료

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,3 +2,4 @@ export { default as MainPage } from './MainPage';
 export { default as LoginPage } from './LoginPage';
 export { default as LandingPage } from './LandingPage';
 export { default as OAuthRedirect } from './OAuthRedirect';
+export { default as AlbumCreatePage } from './AlbumCreatePage';

2e69b0d74d31989711de8eb6f7652ae63d5c8fe4
MyeongHoonNam <codenmh0822@naver.com>
2021-12-15 21:18:07 +0900

    ✨ feat #110 : usePromise 커스텀 훅 구현

---
+++ src/hooks/usePromise.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+const usePromise = (promiseCreator, deps) => {
+  const [loading, setLoading] = useState(false);
+  const [resolved, setResolved] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const process = async () => {
+      setLoading(true);
+
+      try {
+        const resolved = await promiseCreator();
+
+        setResolved(resolved);
+      } catch (e) {
+        setError(e);
+      }
+
+      setLoading(false);
+    };
+
+    process();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deps]);
+
+  return [loading, resolved, error];
+};
+
+export default usePromise;

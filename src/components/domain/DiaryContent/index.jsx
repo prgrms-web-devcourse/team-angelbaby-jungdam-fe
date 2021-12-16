f59fb6c4628c60d4767ab23903531d6cdcdb4696
MyeongHoonNam <codenmh0822@naver.com>
2021-12-17 01:08:42 +0900

    ✨ feat #127 : 일기 상세 내용 데이터 개행 적용 기능 구현

--- src/components/domain/DiaryContent/index.jsx
+++ src/components/domain/DiaryContent/index.jsx
@@ -1,15 +1,23 @@
 import styled from '@emotion/styled';
 import font from '@assets/fonts';
+import color from '@assets/colors';
 
 const Container = styled.div`
+  width: 100%;
   display: flex;
+  flex-direction: column;
   margin-top: 10px;
-  margin-bottom: 20px;
-  ${() => font.content_12}
+  padding-bottom: 20px;
+  border-bottom: 1px solid ${color.grey};
+  ${() => font.content_16}
 `;
 
 const DiaryContent = ({ content }) => {
-  return <Container>{content}</Container>;
+  return (
+    <Container>
+      {content && content.split('\n').map((line) => <p>{line}</p>)}
+    </Container>
+  );
 };
 
 export default DiaryContent;

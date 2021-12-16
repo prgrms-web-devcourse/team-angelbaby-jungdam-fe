db938e88e2965b0958f48293d32187999f64fd81
krungy <furler711@gmail.com>
2021-12-17 04:20:25 +0900

    ✨ feat #132 : 앨범 초대 API 추가

---
+++ src/common/api/MemberList.jsx
@@ -0,0 +1,13 @@
+import api from './api';
+
+export const searchUser = async (email) =>
+  await api({
+    url: `/api/v1/members/search?email=${email}`,
+    method: 'GET',
+  });
+
+export const inviteUser = async (albumId) =>
+  await api({
+    url: `/api/v1/albums/${albumId}/invitations`,
+    method: 'POST',
+  });

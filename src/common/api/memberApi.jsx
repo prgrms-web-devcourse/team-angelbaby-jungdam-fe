6feedb6ee70037293f63fcc9469ce532be80afa7
minsgy <alstjrchl97@naver.com>
2021-12-16 23:54:26 +0900

    ✨ feat #131 : 멤버 수정 API 명시 완료

--- src/common/api/memberApi.jsx
+++ src/common/api/memberApi.jsx
@@ -3,5 +3,12 @@ import api from './api';
 export const getMember = async () =>
   await api({
     url: '/api/v1/members',
-    method: 'get',
+    type: 'get',
+  });
+
+export const putMember = async (params) =>
+  await api({
+    url: '/api/v1/members',
+    type: 'put',
+    params,
   });

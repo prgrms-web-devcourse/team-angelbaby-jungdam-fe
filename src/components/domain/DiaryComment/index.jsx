2fd7101c43a64c9fc86848abb2aa481b7284fcb3
MyeongHoonNam <codenmh0822@naver.com>
2021-12-19 22:25:39 +0900

    ✨ feat #139 : 댓글 삭제 UI 사용자 정보에 따라 동적 구현

--- src/components/domain/DiaryComment/index.jsx
+++ src/components/domain/DiaryComment/index.jsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 import font from '@assets/fonts';
+import color from '@assets/colors';
 import { forwardRef } from 'react';
+import { Icon } from '@components/base';
+import { useSelector } from 'react-redux';
 
 const Conatainer = styled.div`
   padding-top: 20px;
@@ -10,6 +13,8 @@ const Article = styled.div`
   margin-bottom: 30px;
   display: flex;
   flex-direction: row;
+  align-items: center;
+  position: relative;
 `;
 
 const Avatar = styled.img`
@@ -31,29 +36,50 @@ const UserInfo = styled.span`
 `;
 
 const Comment = styled.span`
-  ${() => font.content_12}
+  display: flex;
+  flex-direction: row;
+  align-items: space-between;
+  ${() => font.content_12};
 `;
 
 const Paragraph = styled.p`
   margin-bottom: 0.025px;
 `;
 
+const Delete = styled.button`
+  position: absolute;
+  right: 0;
+  border: solid 1px black;
+  outline: none;
+  border: none;
+  background: none;
+`;
+
 const DiaryComment = forwardRef(({ comments }, ref) => {
+  const userInfo = useSelector((state) => state.member.data.memberEmail);
+
   return (
     <Conatainer ref={ref}>
-      {comments.map(({ commentId, avatar, nickname, commentContent }) => (
-        <Article key={commentId}>
-          <Avatar alt="images" src={avatar} />
-          <CommentContainer>
-            <UserInfo>{nickname}</UserInfo>
-            <Comment>
-              {commentContent.split('\n').map((line, index) => (
-                <Paragraph key={index}>{line}</Paragraph>
-              ))}
-            </Comment>
-          </CommentContainer>
-        </Article>
-      ))}
+      {comments.map(
+        ({ commentId, avatar, nickname, commentContent, email }) => (
+          <Article key={commentId}>
+            <Avatar alt="images" src={avatar} />
+            <CommentContainer>
+              <UserInfo>{nickname}</UserInfo>
+              <Comment>
+                {commentContent.split('\n').map((line, index) => (
+                  <Paragraph key={index}>{line}</Paragraph>
+                ))}
+              </Comment>
+            </CommentContainer>
+            {userInfo === email && (
+              <Delete>
+                <Icon name="fluent:delete-16-filled" color={color.grey} />
+              </Delete>
+            )}
+          </Article>
+        ),
+      )}
     </Conatainer>
   );
 });

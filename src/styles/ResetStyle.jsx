38e1430fb144adfdb8bd01cd59c609707117c60f
MyeongHoonNam <codenmh0822@naver.com>
2021-12-19 22:19:34 +0900

    🎨 style #139 : 전역 스타일 수정

--- src/styles/ResetStyle.jsx
+++ src/styles/ResetStyle.jsx
@@ -137,7 +137,6 @@ const ResetStyle = css`
   }
   * {
     box-sizing: border-box;
-    color: ${color.black};
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
       Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo',
       'Noto Sans KR', 'Malgun Gothic', sans-serif;

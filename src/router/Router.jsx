dfeb89c1e53a89572337b84ae058ad6e7e0cebc4
minsgy <alstjrchl97@naver.com>
2021-12-21 11:27:47 +0900

    🐛 fix : 라우터 랜딩 페이지 로딩 에러

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -34,11 +34,11 @@ const Router = () => {
       <DefaultTemplate>
         <Routes>
           <Route path="/" element={<Navigate to="/tutorial" />} />
+          <Route path="/tutorial" element={<LandingPage />} />
           {/* PreventedRoute - Token이 존재 할 시 자동 메인(앨범 선택)으로 이동 */}
           <Route element={<PreventedRoute />}>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/oauth/redirect" element={<OAuthRedirect />} />
-            <Route path="/tutorial" element={<LandingPage />} />
           </Route>
           {/* AuthRoute - Token이 존재해야 접속 가능함. */}
           <Route element={<AuthRoute />}>

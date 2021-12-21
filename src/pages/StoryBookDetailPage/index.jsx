ed544082c15e8419a7fcc3c7d5ca9ed517bf1f0e
minsgy <alstjrchl97@naver.com>
2021-12-21 11:06:50 +0900

    ✨ feat #161 : 스토리북 더보기 페이지 무한스크롤 구현

--- src/pages/StoryBookDetailPage/index.jsx
+++ src/pages/StoryBookDetailPage/index.jsx
@@ -1,35 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DefaultContainer from '@styles/DefaultContainer';
 import styled from '@emotion/styled';
+import { StoryBookDiaryListContainer } from '@components/domain/StoryBookDiaryList';
+import { Skeleton } from '@components/base';
 import { StoryBookDiaryList, DetailPageHeader } from '@components/domain';
-import { useNavigate } from 'react-router';
-
-const DUMMY_DATA = {
-  avatar: 'https://picsum.photos/200/200',
-  memberName: '구피',
-  diaryList: [
-    {
-      diaryImage: 'https://picsum.photos/200/200',
-      title: '스토리북 제목',
-      dateTime: '2020-01-01',
-    },
-    {
-      diaryImage: 'https://picsum.photos/200/200',
-      title: '스토리북 제목',
-      dateTime: '2020-01-01',
-    },
-    {
-      diaryImage: 'https://picsum.photos/200/200',
-      title: '스토리북 제목',
-      dateTime: '2020-01-01',
-    },
-    {
-      diaryImage: 'https://picsum.photos/200/200',
-      title: '스토리북 제목',
-      dateTime: '2020-01-01',
-    },
-  ],
-};
+import { getStoryBookDetail } from '@api/storyBookApi';
+import { useNavigate, useParams } from 'react-router';
 
 const StoryBookDetailPageContainer = styled(DefaultContainer)`
   height: auto;
@@ -37,20 +13,109 @@ const StoryBookDetailPageContainer = styled(DefaultContainer)`
   box-sizing: content-box;
 `;
 
+const ObserverBox = styled.div`
+  width: 100%;
+  height: 100px;
+`;
+
 const StoryBookDetailPage = () => {
+  const [target, setTarget] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { albumId, participantId } = useParams();
+  const [storybookDetail, setStorybookDetail] = useState({
+    hasNext: true,
+    participantId: '',
+    participantNickname: '',
+    participantAvatar: '',
+    diaries: [],
+  });
+
   const navigate = useNavigate();
+
+  const renderSkeleton = () => {
+    let list = [];
+    for (let i = 0; i < 8; i++) {
+      list.push(<Skeleton.Box width={'100%'} height={'100%'} />);
+    }
+    return list;
+  };
+
+  const renderStoryBookDiary = async (cursorId) => {
+    setIsLoading(true);
+    try {
+      if (!cursorId) {
+        const {
+          data: { data },
+        } = await getStoryBookDetail({ albumId, participantId });
+        setStorybookDetail((prev) => ({ ...prev, ...data }));
+      } else {
+        const {
+          data: { data },
+        } = await getStoryBookDetail({ albumId, participantId, cursorId });
+        setStorybookDetail((prev) => ({
+          ...data,
+          diaries: [...prev.diaries, ...data.diaries],
+        }));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    setIsLoading(false);
+  };
+
+  const onIntersect = ([entry]) => {
+    if (
+      entry.isIntersecting &&
+      !isLoading &&
+      storybookDetail.hasNext &&
+      storybookDetail.diaries.length > 0
+    ) {
+      const { id } =
+        storybookDetail.diaries[storybookDetail.diaries.length - 1];
+      renderStoryBookDiary(id);
+    }
+  };
+
+  useEffect(() => {
+    renderStoryBookDiary();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    let observer;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 1,
+      });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [target, isLoading]);
+
   const handleGoBack = () => {
     navigate(-1);
   };
 
   return (
-    <StoryBookDetailPageContainer>
-      <DetailPageHeader
-        handleGoBack={handleGoBack}
-        pageTitle={`'${DUMMY_DATA.memberName}' 님의 스토리북`}
-      />
-      <StoryBookDiaryList diaryList={DUMMY_DATA.diaryList} />
-    </StoryBookDetailPageContainer>
+    <>
+      <StoryBookDetailPageContainer>
+        <DetailPageHeader
+          handleGoBack={handleGoBack}
+          pageTitle={`'${storybookDetail.participantNickname}' 님의 스토리북`}
+        />
+        <StoryBookDiaryList
+          diaryList={storybookDetail.diaries}
+          albumId={albumId}
+        />
+      </StoryBookDetailPageContainer>
+      {isLoading && (
+        <StoryBookDiaryListContainer>
+          {renderSkeleton()}
+        </StoryBookDiaryListContainer>
+      )}
+      {storybookDetail.hasNext && <ObserverBox ref={setTarget} />}
+    </>
   );
 };
 

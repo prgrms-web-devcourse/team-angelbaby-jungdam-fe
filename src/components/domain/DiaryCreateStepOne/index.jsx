728d6db449ed728f1030235cd7bc0789ed02dcf6
MyeongHoonNam <codenmh0822@naver.com>
2021-12-23 13:30:10 +0900

    🚑 hotfix : 캘린더 기능 및 UI 수정

--- src/components/domain/DiaryCreateStepOne/index.jsx
+++ src/components/domain/DiaryCreateStepOne/index.jsx
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import font from '@assets/fonts';
-import { Input } from '@components/base';
+import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/esm/locale';
+import color from '@assets/colors';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Container = styled.div`
   margin-top: 80px;
@@ -13,23 +18,41 @@ const Title = styled.span`
   ${font.heading_24};
 `;
 
+const InputStyle = css`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid ${color.brown};
+  background-color: transparent;
+  color: ${color.black};
+  outline: none;
+  ${font.content_18};
+
+  &::placeholder {
+    color: ${color.grey};
+  }
+`;
+
 const ErrorText = styled.span`
   margin-top: 5px;
   font-size: 14px;
   color: red;
 `;
 
-const DiaryCreateStepOne = ({ onChange, date, dateError, getTodayDate }) => {
+const DiaryCreateStepOne = ({ date, setDate, dateError }) => {
   return (
     <Container>
       <Title>날짜를 선택해주세요.</Title>
-      <Input
-        type="date"
+      <DatePicker
+        css={InputStyle}
+        locale={ko}
+        dateFormat="yyyy-MM-dd"
         name="date"
-        onChange={onChange}
-        css={font.content_16}
-        max={getTodayDate()}
-        value={date}
+        onChange={(date) => setDate(date)}
+        placeholderText="일기 기록 날짜를 입력해주세요 !"
+        selected={date}
+        maxDate={new Date()}
       />
       {dateError && <ErrorText>{dateError}</ErrorText>}
     </Container>
